efeaf517c362a526f456271ac9d51ecc50655b5b
ennioluwa <enioluwaarowo@gmail.com>
2023-03-12 11:43:44 +0100

    fix bug

--- pages/blog.tsx
+++ pages/blog.tsx
@@ -2,11 +2,7 @@ import React from "react";
 import { Blog } from "../components";
 
 const blog = () => {
-  return (
-    <div>
-      <Blog />
-    </div>
-  );
+  return <div>{/* <Blog post={undefined} otherPosts={[]} /> */}</div>;
 };
 
 export default blog;
