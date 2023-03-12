9268788d15861c6044e2d9dfb2d1a79804690823
ennioluwa <enioluwaarowo@gmail.com>
2023-03-12 19:27:41 +0100

    add click outside to close navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -6,69 +6,23 @@ interface Props {
 const Navbar = ({ text }: Props) => {
   const [open, setOpen] = useState(false);
   return (
-    <nav className="  p-5 sm:p-10">
-      <div
-        className={`relative container mx-auto xl:max-w-7xl flex gap-3 justify-between items-center z-10 ${
-          text ? "text-black" : "text-white"
-        } ${(text === "gallery" || text === "services") && "text-white"}`}
-      >
-        <h1 className=" text-3xl h-16 w-32 lg:h-[56px] lg:w-[250px] grid place-items-center bg-gray-300 rounded-lg ">
-          <Link href="/">Logo</Link>
-        </h1>
-        <ul className=" ml-auto md:flex gap-5 lg:gap-10 xl:gap-[32px] items-center hidden xl:text-[16px] leading-[24px]">
-          <li className={`${text === "services" && "text-primary"} nav-links`}>
-            <Link href="/services">Services</Link>
-          </li>
-          <li className={`${text === "gallery" && "text-primary"} nav-links`}>
-            <Link href="/gallery">Gallery</Link>
-          </li>
-          <li className={`${text === "company" && "text-primary"} nav-links`}>
-            <Link href="/company">Company</Link>
-          </li>
-          <li className={`${text === "contact" && "text-primary"} nav-links`}>
-            <Link href="/contact">Contact Us</Link>
-          </li>
-          <button className="xl:ml-[16px] ml-3 xl:px-[63.5px] p-5 lg:px-10  py-[16px] bg-[#DF4D31] rounded-lg text-[16px] leading-[24px] font-bold text-white ">
-            Get Started
-          </button>
-        </ul>
-        {open ? (
-          <button onClick={() => setOpen(false)} className="block md:hidden">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth={1.5}
-              stroke="currentColor"
-              className="w-8 h-8"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M6 18L18 6M6 6l12 12"
-              />
-            </svg>
-          </button>
-        ) : (
-          <button onClick={() => setOpen(true)} className="block md:hidden">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth={1.5}
-              stroke="currentColor"
-              className="w-8 h-8"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
-              />
-            </svg>
-          </button>
-        )}
-        {open && (
-          <ul className=" absolute inset-x-0  top-20 mt-5 sm:ml-auto rounded-lg p-5 flex flex-col justify-center items-center gap-5 sm:w-[300px] text-white bg-gray-400 md:hidden z-50 ">
+    <>
+      {open && (
+        <div
+          className=" fixed w-screen h-screen"
+          onClick={() => setOpen(false)}
+        ></div>
+      )}
+      <nav className="  p-5 sm:p-10">
+        <div
+          className={`relative container mx-auto xl:max-w-7xl flex gap-3 justify-between items-center z-10 ${
+            text ? "text-black" : "text-white"
+          } ${(text === "gallery" || text === "services") && "text-white"}`}
+        >
+          <h1 className=" text-3xl h-16 w-32 lg:h-[56px] lg:w-[250px] grid place-items-center bg-gray-300 rounded-lg ">
+            <Link href="/">Logo</Link>
+          </h1>
+          <ul className=" ml-auto md:flex gap-5 lg:gap-10 xl:gap-[32px] items-center hidden xl:text-[16px] leading-[24px]">
             <li
               className={`${text === "services" && "text-primary"} nav-links`}
             >
@@ -83,13 +37,75 @@ const Navbar = ({ text }: Props) => {
             <li className={`${text === "contact" && "text-primary"} nav-links`}>
               <Link href="/contact">Contact Us</Link>
             </li>
-            <button className=" px-3 py-2 bg-[#DF4D31] rounded-lg text-[16px] leading-[24px] font-bold ">
+            <button className="xl:ml-[16px] ml-3 xl:px-[63.5px] p-5 lg:px-10  py-[16px] bg-[#DF4D31] rounded-lg text-[16px] leading-[24px] font-bold text-white ">
               Get Started
             </button>
           </ul>
-        )}
-      </div>
-    </nav>
+          {open ? (
+            <button onClick={() => setOpen(false)} className="block md:hidden">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="w-8 h-8"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M6 18L18 6M6 6l12 12"
+                />
+              </svg>
+            </button>
+          ) : (
+            <button onClick={() => setOpen(true)} className="block md:hidden">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="w-8 h-8"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+                />
+              </svg>
+            </button>
+          )}
+          {open && (
+            <ul className=" absolute inset-x-0  top-20 mt-5 sm:ml-auto rounded-lg p-5 flex flex-col justify-center items-center gap-5 sm:w-[300px] text-white bg-gray-400 md:hidden z-50 ">
+              <li
+                className={`${text === "services" && "text-primary"} nav-links`}
+              >
+                <Link href="/services">Services</Link>
+              </li>
+              <li
+                className={`${text === "gallery" && "text-primary"} nav-links`}
+              >
+                <Link href="/gallery">Gallery</Link>
+              </li>
+              <li
+                className={`${text === "company" && "text-primary"} nav-links`}
+              >
+                <Link href="/company">Company</Link>
+              </li>
+              <li
+                className={`${text === "contact" && "text-primary"} nav-links`}
+              >
+                <Link href="/contact">Contact Us</Link>
+              </li>
+              <button className=" px-3 py-2 bg-[#DF4D31] rounded-lg text-[16px] leading-[24px] font-bold ">
+                Get Started
+              </button>
+            </ul>
+          )}
+        </div>
+      </nav>
+    </>
   );
 };
 
